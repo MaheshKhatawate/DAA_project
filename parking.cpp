f64b6be0e968693d9dd89f576e4f34fb5832db80
MaheshKhatawate <01fe23bcs291@kletech.ac.in>
2024-12-25 18:45:34 +0530

    Added feature 1.Warshall 2.Block an unblock parking spot

--- parking.cpp
+++ parking.cpp
@@ -8,24 +8,57 @@ struct node{
     bool hasParking;
     int carSpots;
     int bikeSpots;
+    int blockedCarSpots; 
+    int blockedBikeSpots; 
 };
 typedef node NODE;
 
 class CityGraph{
     unordered_map<string, NODE> nodes;
-    unordered_map<string, unordered_map<string, int>> adjList;
+    unordered_map<string, unordered_map<string, int>> adjList; //This is adjacency list
+    vector<vector<int>> adjMatrix; //This is adjacency matrix
+    vector<string> places;
+    unordered_map<string ,string> userBlockedSpot;//maps users to place they blocked
+
+    void resizeMatrix(){
+        int n=places.size();
+        adjMatrix.resize(n, vector<int>(n,0));
+    }
+
+    int getIndex(const string& place){
+        return find(places.begin(),places.end(), place)-places.begin();
+    }
 
     public:
         //function to add places
         void addPlace(string name, bool hasParking, int carSpots, int bikeSpots){
+            if(nodes.find(name)==nodes.end()){
             nodes[name]={name, hasParking, carSpots, bikeSpots};
+            places.push_back(name);
+            resizeMatrix();
+            }
+            else{
+                cout<<"Place already exists.\n";
+            }
         }
 
         //function to add connection between the places
         //This is not yet perfect
         void addConnection(string place1, string place2, int distance){
+            if(nodes.find(place1)==nodes.end() || nodes.find(place2)==nodes.end()){
+                cout<<"One or both places do not exist.\n";
+                return;
+            }
+
+            //Update adjacency list
             adjList[place1][place2]=distance;
             adjList[place2][place1]=distance;
+
+            //Update adjacency matrix
+            int idx1=getIndex(place1);
+            int idx2=getIndex(place2);
+            adjMatrix[idx1][idx2]=1;
+            adjMatrix[idx2][idx1]=1;
         }
 
         //function to display the details of the city 
@@ -33,7 +66,7 @@ class CityGraph{
             for(auto& [place, data]:nodes){
                 cout<<"Place: "<<place;
                 if(data.hasParking){
-                    cout << " (Parking: Yes, Car Spots: " << data.carSpots<< ", Bike Spots: " << data.bikeSpots << ")";
+                    cout << " (Parking: Yes, Remaining Car Spots: " << data.carSpots-data.blockedCarSpots<< ", Remaining Bike Spots: " << data.bikeSpots-data.blockedBikeSpots << ")";
                 }
                 else{
                     cout<<" (Parking: No)";
@@ -104,17 +137,118 @@ class CityGraph{
             }
             cout<<"No parking slot is available from the current location.\n";
         }
+
+        //function to check whether the path exist between two points
+        void checkPathExistence(const string& place1,const string& place2){
+            if(nodes.find(place1)==nodes.end() || nodes.find(place2)==nodes.end()){
+                cout<<"One or both places do not exist.\n";
+                return;
+            }
+
+            //warshall algorithm
+            vector<vector<int>> closure=adjMatrix;
+            int n=places.size();
+            for(int k=0;k<n;k++){
+                for(int i=0;i<n;i++){
+                    for(int j=0;j<n;j++){
+                        closure[i][j] = closure[i][j] || (closure[i][k] && closure[k][j]);
+                    }
+                }
+            }
+
+            int idx1=getIndex(place1);
+            int idx2=getIndex(place2);
+            if(closure[idx1][idx2]||closure[idx2][idx1]){
+                cout<<"A path exist between "<<place1<<" and "<<place2<<".\n";
+            }
+            else{
+                cout<<"No path exist between "<<place1<<" and "<<place2<<".\n";
+            }
+        }
+
+        //function to block and unblock the parking spot
+        void blockUnblockParkingSpot(const string &user,const string &place, bool isCarSpot, bool block){
+            if(nodes.find(place)==nodes.end()){
+                cout<<"Place does not exist.\n";
+                return;
+            }
+
+            NODE &node=nodes[place];
+            if(!node.hasParking){
+                cout<<"This place does not have parking.\n";
+                return;
+            }
+
+            if(block){
+                //logic to block the spot
+                if(userBlockedSpot.find(user)!=userBlockedSpot.end()){
+                    cout<<"User: "<<user<<" has already blocked a spot at "<<userBlockedSpot[user]<<".\n";
+                    return;
+                }
+
+                if(isCarSpot){
+                    if(node.carSpots>node.blockedCarSpots){
+                        node.blockedCarSpots++;
+                        userBlockedSpot[user]=place;
+                        cout<<"Car parking spot blocked at "<<place<<" by user "<<user<<".\n";
+                    }
+                    else{
+                        cout<<"No available car parking spots to block at "<<place<<".\n"; 
+                    }
+                }
+                else{
+                    if(node.bikeSpots>node.blockedBikeSpots){
+                        node.blockedBikeSpots++;
+                        userBlockedSpot[user]=place;
+                        cout<<"Bike parking spot blocked at "<<place<<" by user "<<user<<".\n";
+                    }
+                    else{
+                        cout<<"No available car parking spots to block at "<<place<<".\n";
+                    }
+                }
+            }
+            else{
+                //unblocking logic
+                if(userBlockedSpot.find(user)==userBlockedSpot.end() || userBlockedSpot[user]!=place){
+                    cout<<"User "<<user<<" has not blocked any spot at "<<place<<".\n";
+                    return;
+                }
+
+                if(isCarSpot){
+                    if(node.blockedCarSpots>0){
+                        node.blockedCarSpots--;
+                        userBlockedSpot.erase(user);
+                        cout<<"Car parking spot unblocked at "<<place<<" by user "<<user<<".\n";
+                    }
+                    else{
+                        cout<<"No blocked car parking spots to unblock at "<<place<<".\n";
+                    }
+                }
+                else{
+                    if(node.blockedBikeSpots>0){
+                        node.blockedBikeSpots--;
+                        userBlockedSpot.erase(user);
+                        cout<<"Bike parking spot unblocked at "<<place<<" by user "<<user<<".\n";
+                    }
+                    else{
+                        cout<<"No blocked car parking spots to unblock at "<<place<<".\n";
+                    }
+                }
+            }
+        }
 };
 
 int main(){
     CityGraph city;
     int choice;
     while(1){
-        cout<<"1-Add Place\n";
+        cout<<"\n1-Add Place\n";
         cout<<"2-Connect Places\n";
         cout<<"3-Display city\n";
         cout<<"4-Find the nearest parking spot\n";
-        cout<<"5-Exit\n";
+        cout<<"5-Check if path exist between two places.\n";
+        cout<<"6-Block or unblock a parking spot.\n";
+        cout<<"7-Exit\n";
         cout<<"Enter your choice:";
         cin>>choice;
 
@@ -170,6 +304,31 @@ int main(){
                 break;
             }
             case 5:{
+                string place1,place2;
+                cin.ignore();
+                cout<<"Enter the first place:";
+                getline(cin,place1);
+                cout<<"Enter the second place:";
+                getline(cin,place2);
+                city.checkPathExistence(place1,place2);
+                break;
+            }
+            case 6:{
+                string user,place;
+                bool isCarSpot, block;
+                cin.ignore();
+                cout<<"Enter your name: ";
+                getline(cin,user);
+                cout<<"Enter the place name:";
+                getline(cin,place);
+                cout<<"Car spot or Bike spot?(1 for car, 0 for bike):";
+                cin>>isCarSpot;
+                cout<<"Block or unblock?(1 from block, 0 for unblock):";
+                cin>>block;
+                city.blockUnblockParkingSpot(user,place,isCarSpot,block);
+                break;
+            }
+            case 7:{
                 cout<<"Exiting the program...\n";
                 return 0;
             }
